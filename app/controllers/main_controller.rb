e29714e9dbfe3da3235498a569193e562eaa167b
Pedro Consuegra <pedroconsuegrat@gmail.com>
2022-01-28 10:30:51 -0500

    fix .env variables

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,9 @@ class MainController < ApplicationController
   layout "blank"
 
     def index
+      @user_name = ENV["GMAIL_USERNAME"]
+      puts "ENV VARIABLE"
+      puts @user_name
       @twitter_count = TwitterAccount.all.count
     end
 
